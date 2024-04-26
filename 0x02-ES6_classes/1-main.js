513ce72911781c63ef7061fde52339008331eb80
hyperayoub <bouagnaayoub@gmail.com>
2024-04-26 15:57:01 +0000

    ES6_classes

---
+++ 0x02-ES6_classes/1-main.js
@@ -0,0 +1,4 @@
+import initializeRooms from './1-make_classrooms.js';
+
+console.log(initializeRooms());
+
---
+++ 0x02-ES6_classes/8-main.js
@@ -0,0 +1,6 @@
+import HolbertonClass from "./8-hbtn_class.js";
+
+const hc = new HolbertonClass(12, "Mezzanine")
+console.log(Number(hc));
+console.log(String(hc));
+

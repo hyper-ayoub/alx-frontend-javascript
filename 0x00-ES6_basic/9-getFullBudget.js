5165557721c64811545a377904c902a1d8713de5
hyperayoub <bouagnaayoub@gmail.com>
2024-04-29 14:43:22 +0000

    a

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -3,16 +3,15 @@ import getBudgetObject from './7-getBudgetObject.js';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
-    income, 
-    gdp,
-    capita,
-    getIncomeInDollars: function (income) {
+    ...budget,
+    getIncomeInDollars(income) {
       return `$${income}`;
     },
-    getIncomeInEuros: function (income) {
+    getIncomeInEuros(income) {
       return `${income} euros`;
     },
   };
 
   return fullBudget;
 }
+

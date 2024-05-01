4604583032adb8c966b123af941a3734590317d5
hyperayoub <bouagnaayoub@gmail.com>
2024-04-30 18:52:00 -0700

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,14 @@
 export const weakMap = new WeakMap();
 
-const hight_End_point = 5;
+const MAX_API = 5; // Renamed the constant
 
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
   weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= hight_End_point) {
+  if (weakMap.get(endpoint) >= MAX_API) { // Updated reference
     throw new Error('Endpoint load is high');
   }
 }
+

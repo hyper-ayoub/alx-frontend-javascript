a669982e1aa59800e0d840d59ed6a0703ed10d8e
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-05-01 01:38:59 +0100

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,6 @@
-// 1-get_list_student_ids.js
- export default function getListStudentIds(arg) {
-  // Check if the argument is an array
-  if (!Array.isArray(arg)) {
-     return arg.map((arg) => arg.id);
+export default function getListStudentIds(arg) {
+  if (Array.isArray(arg)) {
+    return arg.map((arg) => arg.id);
   }
-
   return [];
 }

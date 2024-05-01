8a7be35520a0440cd5c1c2cb0253d0ab03dff120
hyperayoub <bouagnaayoub@gmail.com>
2024-04-30 18:44:58 -0700

    a

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,13 @@
 export const weakMap = new WeakMap();
 
-const MAX_ENDPOINT_CALLS = 5;
+const hight_End_point = 5;
 
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
   weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+  if (weakMap.get(endpoint) >= hight_End_point) {
     throw new Error('Endpoint load is high');
   }
 }

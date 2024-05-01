2b95cc2310688a070e7c257d911b0b0a5e1a9aaa
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-05-01 01:33:35 +0100

    Update 1-get_list_student_ids.js

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+// 1-get_list_student_ids.js
+export default function getListStudentIds(list) {
+  // Check if the argument is an array
+  if (!Array.isArray(list)) {
+    return [];
+  } else {
+    // Use map function to extract ids from objects in the array
+    return list.map(student => student.id);
+  }
+}

157decea2796a129de82cc98bd17e9c85c96c37d
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-05-06 19:56:01 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true; 
   
   if (trueOrFalse) {
-    const task = false; 
-    const task2 = true; 
+    const task = true; 
+    const task2 = false; 
   }
   
   return [task, task2];

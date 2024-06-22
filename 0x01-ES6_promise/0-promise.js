1da2994904c264913d06f650e0739b17f0a01a5c
hyperayoub <bouagnaayoub@gmail.com>
2024-06-22 15:26:58 -0700

    Promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,17 @@
+// 0-promise.js
+
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = true;
+      if (success) {
+        resolve("API response received");
+      } else {
+        reject("API response failed");
+      }
+    }, 2000); // Simulate a delay of 2 seconds
+  });
+}
+
+export default getResponseFromAPI;
+

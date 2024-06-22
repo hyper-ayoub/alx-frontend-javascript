946c610c0a6a07c3986e2f178a513f9b0499908d
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-06-22 23:41:14 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const success = true;
-      if (success) {
-        resolve("API response received");
-      } else {
-        reject("API response failed");
-      }
-    }, 2000);
-  });
+export default  function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {});
 }
-export default function getResponseFromAPI;

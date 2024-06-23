c50ebae45fe8cdee3fe6ef494a5aeca5c99d46d4
hyperayoub <bouagnaayoub@gmail.com>
2024-06-23 13:40:27 -0700

    Promies

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import {uploadPhoto, createUser} from "./utils.js";
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+  .then(([photo, User]) => {
+    const {body} = photo;
+    const {firstName,lastName} = User;
+    console.log(body, firstName, lastName);
+  })
+  .catch(() => {
+    console.log("Signup system offline");
+  });
+}

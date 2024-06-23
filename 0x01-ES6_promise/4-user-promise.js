8a58e333d41daa29eb9c11cdce3380704a0dc069
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-06-23 22:09:21 +0100

    Update 4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,8 @@
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName,
-    lastName,
-  });
+  return Promise.resolve(
+    {
+      firstName,
+      lastName,
+    },
+  );
 }

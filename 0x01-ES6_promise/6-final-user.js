de7d4186340a8a55e239679b432c7a0723f1e0b6
hyperayoub <bouagnaayoub@gmail.com>
2024-09-03 16:32:00 -0700

    handleProfileSignup

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }))
-    ));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((res) => res.map((r) => ({
+    status: r.status,
+    value: r.status === 'fulfilled' ? r.value : String(r.reason),
+  })));
 }

c9054f90192b66f043472ae6477cd95943c9990e
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-09-04 00:18:46 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,14 @@
 import signUpUser from "./4-user-promise.js";
 import uploadPhoto from "./5-photo-reject.js";
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((res) =>
     res.map((r) => ({
       status: r.status,
-      value: r.status === "fulfilled" ? r.value : r.reason,
+      value: r.status === "fulfilled" ? r.value : String(r.reason),
     }))
   );
 }

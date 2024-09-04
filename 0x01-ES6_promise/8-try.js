0460dacc173b34704c57ab26e35b7f43d91de8a3
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-09-04 15:12:09 +0100

    divide

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,7 @@
 /// divideFunction ///
 /// 2 argument numerator && denominator ///
 /// if (denominator == 0) => throw new error cannot divide by 0.///
-/// else return the result denominator/numerator ///
+/// else return the result numerator/denominator ///
 
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {

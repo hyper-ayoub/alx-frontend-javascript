070dfbb7e8febcc0dd2aadb1edeed1e2052a5ee3
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-09-04 02:08:32 +0100

    try && catch && finally

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -13,7 +13,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (err) {
-    queue.push(err);
+    queue.push(String(err));
   } finally {
     queue.push('Guardrail was processed');
   }

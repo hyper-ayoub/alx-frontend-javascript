50fd87f0a7ee701a019a682cfeb7c23588c9e41c
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-09-04 15:10:57 +0100

    load_balancer

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -4,5 +4,5 @@
 // The function should return the value returned by the promise that resolved the first. //
 
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.race([chinaDownload, USDownload]);
+  return Promise.race([chinaDownload, USDownload]); // use race here //
 }

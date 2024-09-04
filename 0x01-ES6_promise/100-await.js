5af81051849c733536f19fad5825887e84f68f90
hyperayoub <bouagnaayoub@gmail.com>
2024-09-04 08:05:39 -0700

    async && await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,13 +1,13 @@
 /// import uploadPhoto && createUser  from utils.js ///
 /// async function => asyncUploadUser ///
-///  call two functions  ///
+/// call two functions  ///
 /// return an object ///
 /// If one of the async function fails, return an empty object. that mean return null ///
 
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  let photo; let  // define the 2 objet //
+  let photo; let
     user;
   try {
     photo = await uploadPhoto(); // use await for function //

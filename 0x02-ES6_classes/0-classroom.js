a3efe74f943d39f7b06daf6e33996c7e3e7356b6
hyperayoub <bouagnaayoub@gmail.com>
2024-09-05 07:12:49 -0700

    classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+// class name  ClassRoom ///
+// accept one attribute named maxStudentsSize (Number) //
+//assigned to _maxStudentsSize//
+
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}

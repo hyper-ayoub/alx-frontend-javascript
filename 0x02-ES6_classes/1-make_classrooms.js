1dc041c0b31e62fc0cb943226f574aa1f053196a
hyperayoub <bouagnaayoub@gmail.com>
2024-09-05 08:27:20 -0700

    class && import

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -6,9 +6,5 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const first = new ClassRoom(19);
-  const second = new ClassRoom(20);
-  const third = new ClassRoom(34);
-  return [first, second, third];
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
-
